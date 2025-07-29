852666010e0e4cb657d0da57f9adc0fb35c42806
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2025-07-29 19:50:17 +0200

    CI updates

--- misc/testsuite/mimic_ctest.sh
+++ misc/testsuite/mimic_ctest.sh
@@ -169,7 +169,7 @@ if [[ $UPX_CONFIG_DISABLE_EXHAUSTIVE_TESTS != ON ]]; then
                 "${run_upx[@]}" -qq -d upx-packed-${s}${exe} ${fo} -o upx-unpacked-${s}${exe}
                 cmp -s upx-unpacked${exe} upx-unpacked-${s}${exe}
                 if [[ $UPX_CONFIG_DISABLE_RUN_PACKED_TEST != ON ]]; then
-                    : ${upx_run_packed_test_count:=9}
+                    : ${upx_run_packed_test_count:=20}
                     for ((i = 0; i < $upx_run_packed_test_count; i++)); do
                         "${emu[@]}" ./upx-packed-${s}${exe} --version-short
                     done
